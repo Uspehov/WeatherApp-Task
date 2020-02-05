3a7e1c1d758a3673e50b3f5930ce4a8f661b5b76
@Andrei_Azarchik <Andrei_Azarchik@epam.com>
2020-02-05 17:23:02 +0300

    third commit

--- src/js/reducers/index.js
+++ src/js/reducers/index.js
@@ -1,29 +1,31 @@
 import { WEATHER } from "../constants/action-types";
 
 const initialState = {
-  weather: {},
   isLoading: true,
   isError: false
 };
 
 function rootReducer(state = initialState, action) {
   if (action.type === WEATHER.GET_WEATHER) {
-    return Object.assign({}, state, {
+    return {
+      //...state,
       isLoading: true,
       isError: false
-    })
+    }
   }
   if (action.type === WEATHER.GET_WEATHER_SUCCES) {
-    return Object.assign({}, state, {
+    return {
+      ...state,
       [action.cityName]: action.response,
       isLoading: false
-    })
+    }
   }
   if (action.type === WEATHER.GET_WEATHER_FAILURE) {
-    return Object.assign({}, state, {
+    return {
+      //...state,
       isLoading: false,
       isError: true
-    })
+    }
   }
 
   return state;
